87b7989c6f0c178aedc8a9322de5e47ab53dad2d
✰ sᴀᴛʏᴀ ʙᴀʙʏ ✰ <tridevk442@gmail.com>
2025-02-01 19:26:04 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -23,7 +23,7 @@ word_db = mongo_client["Word"]["WordDb"]  # Collection for word-response pairs
 # Bot configuration
 API_ID = "16457832"  # Replace with your API ID
 API_HASH = "3030874d0befdb5d05597deacc3e83ab"  # Replace with your API Hash
-BOT_TOKEN = "7242058454:AAE6TakbWWxpS6Xiq-XLXpWPZew9wzxNAu8"  # Replace with your Bot Token
+BOT_TOKEN = "7769757539:AAGwPQa4YvTrMVrQJfLLsubf0qyG5f33-os"  # Replace with your Bot Token
 
 # Initialize bot client
 RADHIKA = Client(
